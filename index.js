cf52edc9ebfa9e0cd5df95bf0d8c9bcd8f155966
SKALEZ-A <holumacbeth@gmail.com>
2024-07-18 11:08:25 +0100

    final

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const { Telegraf, Markup } = require("telegraf");
-const path = require("path");
-const axios = require("axios");
 
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
+
+const { Telegraf } = require("telegraf");
+const axios = require("axios");
+const path = require("path");
+
 const bot = new Telegraf("6809279648:AAH9CZQIpgeVKZWhg3o2zVv0awic9ArJW7Q");
 
-// Helper function to get the image path
 const imagePath = (fileName) => path.join(__dirname, "images", fileName);
 
-// Function to register the user
 const registerUser = async (userId, username, inviterUsername = "") => {
   try {
     const response = await axios.post("https://walledb.onrender.com/api/Cluster0/register", {
@@ -26,7 +25,7 @@ const registerUser = async (userId, username, inviterUsername = "") => {
 bot.start(async (ctx) => {
   const userId = ctx.from.id;
   const username = ctx.from.username || `user${userId}`;
-  const inviterUsername = ctx.startPayload || ""; // Extract inviter username from start payload if available
+  const inviterUsername = ctx.startPayload || "";
 
   try {
     await registerUser(userId, username, inviterUsername);
